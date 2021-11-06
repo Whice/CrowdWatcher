5ac02ab7d744ca54102395a50400bdf2ed43046f
Forni <and-not@mail.ru>
2021-11-06 12:08:54 +0300

    Написана функция, определяющая попадает ли юнит в поле зрения. Без учета дальности.

--- TestTask/TestTask.cpp
+++ TestTask/TestTask.cpp
@@ -15,14 +15,28 @@ struct Point
 /// <summary>
 /// Проверить, находится ли юнит в поле зрения.
 /// </summary>
-/// <param name="centerOfView"></param>
-/// <param name="positionUnit"></param>
+/// <param name="centerOfView">Точка - центр направления взгляда.</param>
+/// <param name="positionUnit">Точка - местонахождение юнита.</param>
+/// <param name="visionAngle">Угол обзора.</param>
+/// <returns></returns>
 bool IsUnitInSight(Point& centerOfView, Point& positionUnit, double& visionAngle)
 {
-    double halfOfVisionAngle = visionAngle * 0.5;
+    //Если позиция юнита "за спиной" у смотрящего.
+    // Позиции не могут быть далеко длпруг от друга из-за ограниченности дальности видимости.
+    // Потому можно перемножать и не боться переполнения.
+    if ((centerOfView.x * positionUnit.x < 0) &&
+        (centerOfView.y * positionUnit.y < 0))
+    {
+        return false;
+    }
+
+    //Найти в радианах половину угла обзора
+    double halfOfVisionAngle = (visionAngle * 0.5)*PI/180;
+    //найти в радианах соответствующие углы зрению и метоположению
     double angleOfView = atan(centerOfView.y / centerOfView.x);
     double angleOfVectorToUnit = atan(positionUnit.y / positionUnit.x);
 
+    //Если разница между углом зрения и местоположением меньше половины угла обзора, то юнит видно.
     if (abs(angleOfView - angleOfVectorToUnit) < halfOfVisionAngle)
     {
         return true;
@@ -36,6 +50,33 @@ bool IsUnitInSight(Point& centerOfView, Point& positionUnit, double& visionAngle
 int main()
 {
     setlocale(LC_ALL, "");
+
+    double visionAngle;
+
+    Point centerOfView;
+    Point positionUnit;
+
+    while (true)
+    {
+        cout << "Input visionAngle: ";
+        cin >> visionAngle;
+
+        
+        cout << "Input centerOfView x: ";
+        cin >> centerOfView.x;
+        cout << "Input centerOfView y: ";
+        cin >> centerOfView.y;
+        cout << "Input positionUnit x: ";
+        cin >> positionUnit.x;
+        cout << "Input positionUnit y: ";
+        cin >> positionUnit.y;
+
+        if (IsUnitInSight(centerOfView, positionUnit, visionAngle))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl << endl;
+    }
     double param = 45.0;                      // угол 45 градусов
 
     std::cout << "Тангенс угла "
