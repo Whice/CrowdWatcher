4c1e4b65e3cdf975fe3d12d8ab3d2e156823652c
Forni <and-not@mail.ru>
2021-11-06 14:00:58 +0300

    Создан класс unit, где реализован учет тех юнитов, которые видны объекту юнита.

--- TestTask/TestTask.cpp
+++ TestTask/TestTask.cpp
@@ -1,127 +1,52 @@
 ﻿#include <iostream>
 #include <conio.h>
 #include <cmath>
+#include "Unit.h"
 
 using namespace std;
 
-const double PI = 3.14159265;
 
-struct Point
-{
-public: Point()
-{
-    this->x = 0;
-    this->y = 0;
-}
-public: Point(double x, double y)
-{
-    this->x = x;
-    this->y = y;
-}
-      double x;
-      double y;
-};
 
-/// <summary>
-/// Проверить, находится ли юнит в поле зрения.
-/// </summary>
-/// <param name="centerOfView">Точка - центр направления взгляда.</param>
-/// <param name="positionUnit">Точка - местонахождение юнита.</param>
-/// <param name="visionAngle">Угол обзора.</param>
-/// <returns></returns>
-bool IsUnitInSight(Point& centerOfView, Point& positionUnit, double& visionAngle)
-{
-    //Если позиция юнита "за спиной" у смотрящего.
-    // Позиции не могут быть далеко длпруг от друга из-за ограниченности дальности видимости.
-    // Потому можно перемножать и не боться переполнения.
-    if ((centerOfView.x * positionUnit.x < 0) &&
-        (centerOfView.y * positionUnit.y < 0))
-    {
-        return false;
-    }
 
-    //Найти в радианах половину угла обзора
-    double halfOfVisionAngle = (visionAngle * 0.5)*PI/180;
-    //найти в радианах соответствующие углы зрению и метоположению
-    double angleOfView = atan(centerOfView.y / centerOfView.x);
-    double angleOfVectorToUnit = atan(positionUnit.y / positionUnit.x);
-
-    //Если разница между углом зрения и местоположением меньше половины угла обзора, то юнит видно.
-    if (abs(angleOfView - angleOfVectorToUnit) < halfOfVisionAngle)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-/// <summary>
-/// Проверить находится ли юнитв пределах коружности или на ее границе.
-/// </summary>
-/// <param name="centerOfCircle">Точка центра окружности.</param>
-/// <param name="positionUnit">Местонахождение юнита.</param>
-/// <param name="radius">Радиус окружности.</param>
-/// <returns>true, если в переделах окружности или на ней самой.</returns>
-bool IsInsideCircle(Point& centerOfCircle, Point& positionUnit, double radius=2)
-{
-    double deltaX = positionUnit.x - centerOfCircle.x;
-    double deltaY = positionUnit.y - centerOfCircle.y;
-
-    //При хорошем разбросе юнитов эта ситуация будет встречаться чаще всего.
-    if (abs(deltaX) > radius || abs(deltaY) > radius)
-        return false;
-
-    //Если юнит все-таки где-то рядом, тогда проверить в границе ли он или на ней ли.
-    if (deltaX * deltaX + deltaY * deltaY <= radius * radius)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-/// <summary>
-/// Проверяет находится ли юнит в поле видимости наблюдателя.
-/// </summary>
-/// <param name="observerLocation">Позиция наблюдателя.</param>
-/// <param name="centerOfView">Точка - центр направления взгляда.</param>
-/// <param name="positionUnit">Точка - местонахождение юнита.</param>
-/// <param name="visionAngle">Угол обзора. Размер сектора в градусах.</param>
-/// <param name="radius">Радиус окружности. Дальность видимости.</param>
-/// <returns></returns>
-bool UnitIsVisible(Point& observerLocation, Point& centerOfView, Point& positionUnit, double visionAngle = 135.5, double radius = 2)
-{
-    return IsUnitInSight(centerOfView, positionUnit, visionAngle) & IsInsideCircle(observerLocation, positionUnit, radius);
-}
 
 int main()
 {
     setlocale(LC_ALL, "");
+    double visionAngle = 135.5;
 
-    double visionAngle;
 
-    Point centerOfView;
+    /*
+    Юнит1, позиция (1,1), направление (0,1)
+    Юнит2, позиция (1,2), направление (1,0)
+    Юнит3, позиция (-5,-1), направление (0.707, 0.707)*/
+    vector<Unit> units(3);
+    units[0] = Unit(Point(1, 1), Point(0, 1));
+    units[1] = Unit(Point(1, 2), Point(1,0));
+    units[2] = Unit(Point(-5, -1), Point(0.707, 0.707));
+
+
+    Point directionOfSight;
     Point positionUnit;
     Point observerLocation = Point(0, 0);
     
+    Unit testUnit = Unit();
+
     while (true)
     {
         cout << "Input visionAngle: ";
         cin >> visionAngle;
-
+        visionAngle = visionAngle * PI / 180;
         
         cout << "Input centerOfView x: ";
-        cin >> centerOfView.x;
+        cin >> testUnit.directionOfSight.x;
         cout << "Input centerOfView y: ";
-        cin >> centerOfView.y;
+        cin >> testUnit.directionOfSight.y;
         cout << "Input positionUnit x: ";
         cin >> positionUnit.x;
         cout << "Input positionUnit y: ";
         cin >> positionUnit.y;
 
-        if (UnitIsVisible(observerLocation, centerOfView, positionUnit, visionAngle))
+        if (testUnit.UnitIsVisible(positionUnit, visionAngle))
         {
             cout << "Yes";
         }
---
+++ TestTask/Unit.h
@@ -0,0 +1,141 @@
+#pragma once
+#include "Point.h"
+#include <vector>
+
+const double PI = 3.14159265;
+
+/// <summary>
+/// Класс определяющий юнита.
+/// </summary>
+class Unit
+{
+public:
+	/// <summary>
+	/// Создать юнита, задав ему позицию и направление взгляда.
+	/// </summary>
+	/// <param name="location">Позиция юнита.</param>
+	/// <param name="directionOfSight">Точка, куда направлен взгляд юнита. Центр сектора.</param>
+	Unit(Point location=Point(0, 0), Point directionOfSight= Point(0, 0))
+	{
+		this->location = location;
+		this->directionOfSight = directionOfSight;
+	}
+	/// <summary>
+	/// Позиция юнита.
+	/// </summary>
+	Point location;
+	/// <summary>
+	/// Точка, куда направлен взгляд юнита. Центр сектора.
+	/// </summary>
+	Point directionOfSight;
+    /// <summary>
+    /// Номера юнитов, которые видны этому юниту.
+    /// </summary>
+	std::vector<int> numbersUnitsInDirectionOfSight;
+
+#pragma region Вычисление видимости
+
+private:
+    /// <summary>
+/// Проверить, находится ли юнит в поле зрения.
+/// </summary>
+/// <param name="positionUnit">Точка - местонахождение искомого юнита.</param>
+/// <param name="halfOfVisionAngleInRadians">Угол обзора в радианах.</param>
+/// <returns></returns>
+    bool IsUnitInSight(Point& positionUnit, double& halfOfVisionAngleInRadians )
+    {
+        //Сдвинуть все точки так, чтобы этот юнит был в координатах 0;0
+        int shiftX = this->location.x;
+        int shiftY = this->location.y;
+        Point shiftDirectionOfSight = Point(this->directionOfSight.x-shiftX, this->directionOfSight.y-shiftY);
+        Point shiftPositionUnit = Point(positionUnit.x - shiftX, positionUnit.y - shiftY);
+
+
+        //Если позиция юнита "за спиной" у смотрящего.
+        // Позиции не могут быть далеко длпруг от друга из-за ограниченности дальности видимости.
+        // Потому можно перемножать и не боться переполнения.
+        if ((shiftDirectionOfSight.x * shiftPositionUnit.x < 0) &&
+            (shiftDirectionOfSight.y * shiftPositionUnit.y < 0))
+        {
+            return false;
+        }
+        //найти в радианах соответствующие углы зрению и метоположению
+        double angleOfView = atan(shiftDirectionOfSight.y / shiftDirectionOfSight.x);
+        double angleOfVectorToUnit = atan(shiftPositionUnit.y / shiftPositionUnit.x);
+
+        //Если разница между углом зрения и местоположением меньше половины угла обзора, то юнит видно.
+        if (abs(angleOfView - angleOfVectorToUnit) < halfOfVisionAngleInRadians)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    /// <summary>
+    /// Проверить находится ли юнит в пределах коружности или на ее границе.
+    /// </summary>
+    /// <param name="positionUnit">Местонахождение юнита.</param>
+    /// <param name="radius">Радиус окружности.</param>
+    /// <returns>true, если в переделах окружности или на ней самой.</returns>
+    bool IsInsideCircle(Point& positionUnit, double& radius)
+    {
+        double deltaX = positionUnit.x - this->location.x;
+        double deltaY = positionUnit.y - this->location.y;
+
+        //При хорошем разбросе юнитов эта ситуация будет встречаться чаще всего.
+        if (abs(deltaX) > radius || abs(deltaY) > radius)
+            return false;
+
+        //Если юнит все-таки где-то рядом, тогда проверить в границе ли он или на ней ли.
+        if (deltaX * deltaX + deltaY * deltaY <= radius * radius)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+public:
+    /// <summary>
+    /// Проверяет находится ли указанный юнит в поле видимости этого юнита.
+    /// </summary>
+    /// <param name="positionUnit">Точка - местонахождение юнита.</param>
+    /// <param name="visionAngle">Угол обзора в радианах. Размер сектора.</param>
+    /// <param name="radius">Радиус окружности. Дальность видимости.</param>
+    /// <returns></returns>
+    bool UnitIsVisible(Point& positionUnit, double halfOfVisionAngleInRadians = (135.5 * PI) / 360/*135.5/2 * PI / 180*/,
+        double radius = 2)
+    {
+        bool sight = IsUnitInSight(positionUnit, halfOfVisionAngleInRadians);
+        bool inCircle = IsInsideCircle(positionUnit, radius);
+        return  sight&inCircle ;
+    }
+
+    void CalculateUnitsVisibilityForThisUnit(std::vector<Unit>& listOfUnits, int selfNumberInList)
+    {
+        std::vector<int> numbersVisibilityUnits;
+        int countOfUnits = listOfUnits.size();
+        for (int i = 0; i < selfNumberInList; ++i)
+        {
+            if (this->UnitIsVisible(listOfUnits[i].location))
+            {
+                numbersVisibilityUnits.push_back(i);
+            }
+        }
+        for (int i = selfNumberInList+1; i < countOfUnits; ++i)
+        {
+            if (this->UnitIsVisible(listOfUnits[i].location))
+            {
+                numbersVisibilityUnits.push_back(i);
+            }
+        }
+        this->numbersUnitsInDirectionOfSight = numbersVisibilityUnits;
+    }
+
+#pragma endregion Вычисление видимости
+};
+
